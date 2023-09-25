04fdf438e59c067b9ecba6f04cf9b45b28b35fe5
Karyn <51198063+KarynW13@users.noreply.github.com>
2023-09-25 17:12:49 +0100

    Add files via upload

---
+++ CoinFlip.py
@@ -0,0 +1,17 @@
+import random
+def flip_coin():
+    random_number = random.randint(1, 2)
+    if random_number == 1:
+        side = 'heads'
+    else:
+        side = 'tails'
+    return side
+
+choice = input('heads or tails: ')
+result = flip_coin()
+print('The coin landed on {}'.format(result))
+if choice == result:
+    print("You win")
+else choice != result:
+    print("You lose")
+
